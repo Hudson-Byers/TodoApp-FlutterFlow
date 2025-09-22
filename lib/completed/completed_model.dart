293e6ddc8f58b0d2770ae33f764c2d48fa80e93b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-09-22 13:33:15 +0000

    Updating to latest FlutterFlow output.

--- lib/completed/completed_model.dart
+++ lib/completed/completed_model.dart
@@ -4,10 +4,6 @@ import 'completed_widget.dart' show CompletedWidget;
 import 'package:flutter/material.dart';
 
 class CompletedModel extends FlutterFlowModel<CompletedWidget> {
-  ///  Local state fields for this page.
-
-  bool delete = false;
-
   @override
   void initState(BuildContext context) {}
 
